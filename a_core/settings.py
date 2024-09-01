7945b1f3c0e6e822c2fa72c80eaa732945ed35e9
abrs7 <abrishsambong@gmail.com>
2024-09-01 21:44:24 +0300

    Updated allowed host

--- a_core/settings.py
+++ a_core/settings.py
@@ -30,9 +30,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False    
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://django-docker-beginner.onrender.com']
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://django-docker-beginner.onrender.com/' ]
 
 
 # Application definition
