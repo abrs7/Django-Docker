ffd7b0f6e5afc110b1cce4da7a3ba1bde834c7fd
abrs7 <abrishsambong@gmail.com>
2024-09-02 11:10:16 +0300

    updated the settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -30,9 +30,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False    
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://django-docker-cdm3.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-docker-cdm3.onrender.com']
 
-CSRF_TRUSTED_ORIGINS = [ 'https://django-docker-cdm3.onrender.com' ]
+CSRF_TRUSTED_ORIGINS = [ 'django-docker-cdm3.onrender.com' ]
 
 
 # Application definition
