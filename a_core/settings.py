03e5da692ddb965df095c10c498964ba8d617832
abrs7 <abrishsambong@gmail.com>
2024-09-02 22:15:10 +0300

    testing

--- a_core/settings.py
+++ a_core/settings.py
@@ -33,7 +33,7 @@ else:
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-docker-cdm3.onrender.com', 'django-docker-latest.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [ 'https://django-docker-cdm3.onrender.com' ,'https://django-docker-latest.onrender.com']
-
+## Testing 
 
 # Application definition
 
